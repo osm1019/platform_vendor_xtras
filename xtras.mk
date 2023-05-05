bea8eb12160548e5c1bb4292dd0aba9312dcf922
rdx420 <padraramesh420@gmail.com>
2023-05-05 19:25:58 +0530

    Themes: IconShape: Add more Icon Shapes from `Iconify`
    Credit - https://github.com/Mahmud0808/Iconify

--- xtras.mk
+++ xtras.mk
@@ -118,7 +118,11 @@ PRODUCT_PACKAGES += \
     IconShapeSquircleOverlay \
     IconShapeTaperedRectOverlay \
     IconShapeTeardropOverlay \
-    IconShapeVesselOverlay
+    IconShapeVesselOverlay \
+    IconShapeScrollOverlay \
+    IconShapeIosOverlay \
+    IconShapeRohieMeowOverlay \
+    IconShapeLeafyOverlay
 
 # Prebuilt packages
 PRODUCT_PACKAGES += \
